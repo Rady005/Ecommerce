83d4f9d5e5d476365fc365113311ae5064fccc17
Ren_Rady <Rady005>
2025-01-08 09:52:07 +0700

    Orderscreen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
 import Flutter
 import UIKit
+#import "GoogleMaps/GoogleMaps.h"
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    
   ) -> Bool {
+    [GMSServices provideAPIKey:@"AIzaSyCrdMvvyx8Jym9a3WKN3DOLrKRbT63bgyU"];
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
